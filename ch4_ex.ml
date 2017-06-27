7e280af4dca6ea2e541c987046e00b4f4ec4683b
elzibus <elzibus@users.noreply.github.com>
2017-06-27 22:32:33 +0200

    chapter 4 exercises file

---
+++ ch4_ex.ml
@@ -0,0 +1,11 @@
+
+(* the authors solutions can be found here for all but exercise 4.6 :
+   http://pauillac.inria.fr/cousineau-mauny/sols-chap4.txt
+ *)
+
+(* TODO check that they work in Ocaml *)
+
+(* exercise 4.6 *)
+
+(* TODO *)
+
